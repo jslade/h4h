6d49b55d40726986d17d680e1fb5585a52a8939d
Jeremy Slade <jeremy@jkslade.net>
2024-12-23 04:24:26 -0700

    auto-reload

--- backend/repl.py
+++ backend/repl.py
@@ -4,9 +4,17 @@ from datetime import date, datetime, time, timedelta  # noqa: F401
 from decimal import Decimal  # noqa: F401
 from zoneinfo import ZoneInfo  # noqa: F401
 
+import IPython
+
 from hfh.app import APP
 from hfh.db import DB, DbSession  # noqa: F401
 from hfh.models.all import *  # noqa: F403
 from hfh.services.asic_service import *  # noqa: F403
+
+# IPython setup -- enable auto-reload of source files
+ipython = IPython.get_ipython()
+ipython.run_line_magic(magic_name="load_ext", line="autoreload")
+ipython.run_line_magic(magic_name="autoreload", line="2")
+
 
 APP.app_context().push()
